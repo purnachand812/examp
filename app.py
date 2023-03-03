520fd48d041b3fb394e10413dfbc14929a81b579
purnachand dandu <50629828+purnachand812@users.noreply.github.com>
2023-03-03 18:24:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+from streamlit_lottie import st_lottie
+
+
+st.header('Dandu Purnachandra Madhukar')
+
+st.subheader('student')
+
+with st.container():
+
+    st.subheader('Education')
+    
